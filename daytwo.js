c8ad9410c27f2a17df6d3713295ee00a054104a5
Govinda Sagar Bhusal <167353183+govindasagarbhusal@users.noreply.github.com>
2024-07-18 23:17:08 +0545

    operators

---
+++ daytwo.js
@@ -0,0 +1,83 @@
+// let a = 2;
+// let b = 2;
+// let sum = a + b;
+// console.log(sum);
+
+// let a2 = 2;
+// let b2 = "2";
+// let sum2 = a2 + b2;
+// console.log(sum2);
+
+// let c = 4;
+// let d = 2;
+// let diff = c - d;
+// console.log(diff);
+
+// let c2 = 4;
+// let d2 = "2";
+// let diff2 = c2 - d2;
+// console.log(diff2);
+
+// function findRemainder(dividend, divisor){
+//     return dividend % divisor;
+// }
+
+// let dividend = 12;
+// let divisor = 5;
+// let remainder = findRemainder(dividend, divisor);
+
+// console.log(remainder);
+
+// let a = 2;
+// let b = 3;
+// let sum = a += b;
+// console.log(sum)
+
+// const a = 5;
+// const b = 10;
+// let compare = b > a;
+// console.log(compare);
+// let compare2 = a > b;
+// console.log(compare2);
+
+// const c = 5;
+// const d = 10;
+// let comparee = c < d;
+// console.log(comparee);
+// let comparee2 = d < c;
+// console.log(comparee2);
+
+// const c = 5;
+// const d = 5;
+// let comparee = c <= d;
+// console.log(comparee);
+// let comparee2 = d <= c;
+// console.log(comparee2);
+
+// const c = 5;
+// const d = 5;
+// let comparee = c == d;
+// console.log(comparee);
+// let comparee2 = c === d;
+// console.log(comparee2);
+
+// const a = 5;
+// const b = 10;
+// let compare = a == b;
+// console.log(compare);
+// let compare2 = a === b;
+// console.log(compare2);
+
+// function isPositiveAndEven(number){
+//     if (number > 0 && number % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// const testNumbers = [-5, 0, 2, 3, 4, 7,  10];
+
+// testNumbers.forEach(num => {
+//     console.log(`Is ${num} positive and even? ${isPositiveAndEven(num)}`);
+// });
