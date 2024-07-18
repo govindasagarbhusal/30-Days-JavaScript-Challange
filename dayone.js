9a7755990212690e72aace3fdd40587282358c50
Govinda Sagar Bhusal <167353183+govindasagarbhusal@users.noreply.github.com>
2024-07-18 23:15:42 +0545

    variables and datatypes

---
+++ dayone.js
@@ -0,0 +1,35 @@
+// var age = 12;
+// console.log(age);
+// console.log(typeof age);
+
+
+// let name = "aayush";
+// console.log(name);
+// console.log(typeof name);
+
+
+// const isTrue = true;
+// const isFalse = false;
+// console.log(isFalse);
+// console.log(typeof isFalse);
+
+// const person = {
+//     firstName: "aayush",
+//     lastName: "bhusal",
+//     age: 20,
+//     isEmployed: false
+// };
+
+// console.log(typeof person["isEmployed"])
+// console.log(typeof person.lastName)
+
+// let name = "jeevan";
+// console.log(name);
+// name = "misson"
+// console.log(name);
+
+// const name = "aayush";
+// console.log(name);
+// name = "govinda";
+// console.log(name);
+
